d91232451afd4b830b6da53cf29cce7e60692482
nafisaa11 <jnafisah50@gmail.com>
2026-02-07 13:35:36 +0700

    feat: initialize SvelteKit project with Tailwind CSS and TypeScript

---
+++ src/lib/types.ts
@@ -0,0 +1,52 @@
+interface Dimension {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+interface Reviews {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+interface Meta {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+}
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: Dimension;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Reviews[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: Meta;
+  images: string[];
+  thumbnail: string;
+}
+
+interface ProductResponse{
+    products: Product[];
+    total: number;
+    skip: number;
+    limit: number;
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="text-xl font-bold ">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
